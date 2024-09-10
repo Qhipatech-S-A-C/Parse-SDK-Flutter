2bc6064a3d57222ef466bf9a3a36fa19927e4ad6
bran-x <branelectronica@gmail.com>
2024-09-10 17:50:14 -0500

    update

--- packages/dart/lib/src/objects/parse_polygon.dart
+++ packages/dart/lib/src/objects/parse_polygon.dart
@@ -1,9 +1,9 @@
 part of '../../parse_server_sdk.dart';
 
 class ParsePolygon {
-  List<ParseGeoPoint> coordinates;
-
   ParsePolygon({this.coordinates = const []});
+
+  List<ParseGeoPoint> coordinates;
 
   Map<String, dynamic> toJson({bool full = false, bool forApiRQ = false}) {
     final List<List<double>> points = <List<double>>[];
@@ -11,7 +11,10 @@ class ParsePolygon {
       points.add(<double>[point.latitude, point.longitude]);
     }
 
-    return <String, dynamic>{'__type': 'Polygon', 'coordinates': points};
+    return <String, dynamic>{
+      '__type': 'Polygon',
+      'coordinates': points,
+    };
   }
 
   @override
